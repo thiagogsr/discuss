f2175aeb80fbdc4cda03ad397b6f935e7cba8aca
Thiago Guimarães <thiagogsr@gmail.com>
2017-08-25 17:54:01 -0300

    Fix comment association with user

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -4,7 +4,7 @@ defmodule Discuss.Comment do
   schema "comments" do
     field :content, :string
     belongs_to :topic, Discuss.Topic
-    belongs_to :comment, Discuss.Comment
+    belongs_to :user, Discuss.User
 
     timestamps()
   end
