5430b609f8b530e03bc776fd636e8a12b20b0511
Thiago Guimarães <thiagogsr@gmail.com>
2017-08-28 12:32:14 -0300

    Converts CommentSocket to a js module

---
+++ web/static/js/comment_socket.js
@@ -0,0 +1,82 @@
+import {Socket} from "phoenix"
+import {_} from "underscore"
+
+var CommentSocket = (function(global) {
+  function CommentSocket() {
+    this.socket = new Socket("/socket", { params: { token: global.userToken } })
+    this.channel = this.socket.channel("comment:all", {})
+    this.commentField = document.querySelector("#add-comment")
+    this.submitComment = document.querySelector("#submit-comment")
+    this.comments = document.querySelector("#comments")
+  }
+
+  var fn = CommentSocket.prototype
+
+  fn.init = () => {
+    if (!this.commentField) {
+      return
+    }
+    this.socket.connect()
+    this._bindAll()
+    this._joinChannel()
+    this._loadComments()
+  }
+
+  fn._bindAll = () => {
+    this.commentField.addEventListener("keypress", _.bind(this._submitComment, this))
+    this.submitComment.addEventListener("click", _.bind(this._addComment, this))
+    this.channel.on("load_comments", _.bind(this._renderComments, this))
+    this.channel.on("new_comment", _.bind(this._appendComment, this))
+  }
+
+  fn._joinChannel = () => {
+    this.channel.join()
+      .receive("ok", resp => { console.log("Joined successfully", resp) })
+      .receive("error", resp => { console.log("Unable to join", resp) })
+  }
+
+  fn._loadComments = () => {
+    this.channel.push("load_comments", { topic_id: this._topicId() })
+  }
+
+  fn._submitComment = event => {
+    if(event.keyCode === 13) {
+      this._addComment()
+    }
+  }
+
+  fn._topicId = () => {
+    return global.location.pathname.split('/')[1]
+  }
+
+  fn._addComment = () => {
+    if (this.commentField.value === "") {
+      return
+    }
+
+    var body = {
+      content: this.commentField.value,
+      topic_id: this._topicId()
+    }
+
+    this.channel.push("new_comment", body)
+    this.commentField.value = ""
+  }
+
+  fn._appendComment = payload => {
+    var comment = document.createElement("div")
+    comment.innerHTML = `${payload.content}<br /><small>by ${payload.user_email} at ${payload.inserted_at}</small>`
+    this.comments.appendChild(comment)
+  }
+
+  fn._renderComments = payload => {
+    var comments = payload.comments
+    for (var index in comments) {
+      this._appendComment(comments[index])
+    }
+  }
+
+  return CommentSocket
+})(window);
+
+export default CommentSocket
